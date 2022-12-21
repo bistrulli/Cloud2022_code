7d7d3383e480854a77faf4f1141bda6f78cb9b2c
emilio-imt <emilio-imt@MacBook-Pro.homenet.telecomitalia.it>
2022-12-21 22:04:54 +0100

    minor

--- application/facedetect_3s/apps/build_and_push.sh
+++ application/facedetect_3s/apps/build_and_push.sh
@@ -3,9 +3,9 @@
 export GITLAB_REGISTRY=hub.docker.com # Insert git container registry, we used gitlab
 export REGPATH=bistrulli # Path to the registry
 
-docker login $GITLAB_REGISTRY
+docker login
 
 for d in */ ; do
-  docker build -t $GITLAB_REGISTRY/$REGPATH/"${d///}" $d.
-  docker push $GITLAB_REGISTRY/$REGPATH/"${d///}"
+  docker build -t $REGPATH/"${d///}" $d.
+  docker push $REGPATH/"${d///}"
 done
